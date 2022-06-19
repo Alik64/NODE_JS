287f79428ef450d1f0848d695e8a3b0e30dff450
Alik64 <albert.barsamov@gmail.com>
2022-06-19 17:01:22 +0200

    create data access layer + data.json

--- WOD/src/controllers/workoutController.js
+++ WOD/src/controllers/workoutController.js
@@ -1,23 +1,30 @@
+const workoutSevice = require("../services/workoutService.js");
+
 const getAllWorkouts = (req, res) => {
-  res.send("Get all Workouts");
+  const allWorkouts = workoutSevice.getAllWorkouts();
+  res.send({ status: "OK", data: allWorkouts });
 };
 
 const getOneWorkout = (req, res) => {
-  console.log(req.params);
+  //   console.log(req.params);
+  const workout = workoutSevice.getOneWorkout();
   res.send(
     `Get an existing workout by it's id : <span style="font-size:1.2rem; color:blue">${req.params.workoutId}</span>`
   );
 };
 
 const createNewWorkout = (req, res) => {
+  const createdWorkout = workoutSevice.createNewWorkout();
   res.send("Create a new workout");
 };
 
 const updateOneWorkout = (req, res) => {
+  const updatedWorkout = workoutSevice.updateOneWorkout();
   res.send("Update an existing workout");
 };
 
 const deleteOneWorkout = (req, res) => {
+  const deletedWorkout = workoutSevice.deleteOneWorkout();
   res.send("Delete an existing workout");
 };
 
