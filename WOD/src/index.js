e0b8faea39fd77b90a50b2aa7d987d0817a63f15
Alik64 <albert.barsamov@gmail.com>
2022-06-20 19:04:19 +0200

    install cors

--- WOD/src/index.js
+++ WOD/src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const v1workoutRouter = require("./v1/routes/workoutRoutes");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1/workouts", v1workoutRouter);
+app.use("/api/v1/workouts", cors(), v1workoutRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is deployed on port ${PORT} :)`);
