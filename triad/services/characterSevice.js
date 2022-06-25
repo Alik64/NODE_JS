089a01170cbaa188064959e27e7bc9c904e54d17
Alik64 <albert.barsamov@gmail.com>
2022-06-25 22:56:23 +0200

    create characterService

---
+++ triad/services/characterSevice.js
@@ -0,0 +1,30 @@
+const Character = require("../database/Character.js");
+
+const getAllCharacters = () => {
+  const allCharacters = Character.getAllCharacters();
+  return allCharacters;
+};
+
+const getOneCharacter = () => {
+  return;
+};
+
+const createNewCharacter = () => {
+  return;
+};
+
+const updateOneCharacter = () => {
+  return;
+};
+
+const deleteOneCharacter = () => {
+  return;
+};
+
+module.exports = {
+  getAllCharacters,
+  getOneCharacter,
+  createNewCharacter,
+  updateOneCharacter,
+  deleteOneCharacter,
+};
