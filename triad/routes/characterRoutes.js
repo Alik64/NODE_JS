dad00d408af1785d89d7e9bdbb9fc231f8ffb4bf
Alik64 <albert.barsamov@gmail.com>
2022-06-25 23:01:20 +0200

    update charactersRoutes

--- triad/routes/characterRoutes.js
+++ triad/routes/characterRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 const characterController = require("../controllers/characterController.js");
 
 router.get("/", characterController.getAllCharacters);
