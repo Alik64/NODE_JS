3d19a2b97d7dfcd908a71711a6aa3213e4b9439c
Alik64 <albert.barsamov@gmail.com>
2022-06-25 22:41:58 +0200

    create charactersRoutes.js

---
+++ triad/routes/characterRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.router();
+const characterController = require("");
+
+router.get("/", characterController.getAllCharacters);
+router.get("/:characterId", characterController.getOneCharacter);
+router.post("/", characterController.createNewCharacter);
+router.patch("/characterId", characterController.updateOneCharacter);
+router.delete("/characterId", characterController.deleteOneCharacter);
+
+module.exports = router;
