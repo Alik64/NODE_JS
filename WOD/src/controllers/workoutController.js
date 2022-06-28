bf23778baf19013cc6dd282cd4c048e97db92c10
Alik64 <albert.barsamov@gmail.com>
2022-06-28 16:16:15 +0200

    refactoring workoutController

--- WOD/src/controllers/workoutController.js
+++ WOD/src/controllers/workoutController.js
@@ -13,13 +13,7 @@ const getOneWorkout = (req, res) => {
 const createNewWorkout = (req, res) => {
   const { body } = req;
 
-  if (
-    !body.name ||
-    !body.mode ||
-    !body.equipment ||
-    !body.exercises ||
-    !body.trainerTips
-  ) {
+  if (!body.name || !body.mode || !body.equipment || !body.exercises) {
     return;
   }
 
@@ -28,7 +22,6 @@ const createNewWorkout = (req, res) => {
     mode: body.mode,
     equipment: body.equipment,
     exercises: body.exercises,
-    trainerTips: body.trainerTips,
   };
 
   const createdWorkout = workoutService.createNewWorkout(newWorkout);
