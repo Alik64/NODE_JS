09378b8f34a1966d656b985c92dc86fab95254d4
Alik64 <albert.barsamov@gmail.com>
2022-07-03 22:34:49 +0200

    implement error handling of all methods in Character.js

--- triad/src/v1/database/Character.js
+++ triad/src/v1/database/Character.js
@@ -9,19 +9,94 @@ const getAllCharacters = () => {
   }
 };
 
-const createNewCharacter = (newCharacter) => {
-  const isAlreadyAdded =
-    DB.characters.findIndex((character) => character.id === newCharacter.id) >
-    -1;
-
-  if (isAlreadyAdded) {
-    return "This workout is already exist";
+const getOneCharacter = (characterId) => {
+  try {
+    const character = DB.characters.find(
+      (character) => character.id === characterId
+    );
+    if (typeof character === "undefined") {
+      throw {
+        status: 400,
+        message: `Can't find a character with id ${characterId}`,
+      };
+    }
+  } catch (error) {
+    throw {
+      status: error?.status || 500,
+      message: error?.message || error,
+    };
   }
+};
+const createNewCharacter = (newCharacter) => {
+  try {
+    const isAlreadyAdded =
+      DB.characters.findIndex(
+        (character) => character.name === newCharacter.name
+      ) > -1;
 
-  DB.characters.push(newCharacter);
-  saveToDatabase(DB);
+    if (isAlreadyAdded) {
+      throw {
+        status: 400,
+        message: `Character with the name ${character.name} already exist.`,
+      };
+    }
 
-  return newCharacter;
+    DB.characters.push(newCharacter);
+    saveToDatabase(DB);
+
+    return newCharacter;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
-module.exports = { getAllCharacters, createNewCharacter };
+const updateOneCharacter = (characterId, changes) => {
+  try {
+    const indexForUpdate = DB.characters.findIndex(
+      (character) => character.id === characterId
+    );
+    if (indexForUpdate === -1) {
+      throw {
+        status: 400,
+        message: `Can't find a character with id : ${characterId}`,
+      };
+    }
+    const updatedCharacter = {
+      ...DB.characters[indexForUpdate],
+      ...changes,
+      updateAt: new Date().toLocaleString("fr-Fr", {
+        timeZone: "Europe/Paris",
+      }),
+    };
+    DB.characters[indexForUpdate] = updatedCharacter;
+    saveToDatabase(DB);
+    return updatedCharacter;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
+const deleteOneCharacter = (characterId) => {
+  try {
+    const indexForDeleting = DB.characters.findIndex(
+      (character) => character.id === characterId
+    );
+    if (indexForDeleting === -1) {
+      throw {
+        status: 400,
+        message: `Can't find character with id : ${characterId}`,
+      };
+    }
+    DB.characters.splice(indexForDeleting, 1);
+    saveToDatabase(DB);
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
+
+module.exports = {
+  getAllCharacters,
+  getOneCharacter,
+  createNewCharacter,
+  updateOneCharacter,
+  deleteOneCharacter,
+};
