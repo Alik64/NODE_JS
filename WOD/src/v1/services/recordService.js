d2fe89527c7bc3aabb5699d41c837f71ad135065
Alik64 <albert.barsamov@gmail.com>
2022-07-05 23:09:04 +0200

    implement getRecordforWorkout function in recordService.js

---
+++ WOD/src/v1/services/recordService.js
@@ -0,0 +1,11 @@
+const Record = require("../database/Record.js");
+
+const getRecordForWorkout = (workoutId) => {
+  try {
+    const record = Record.getRecordForWorkout(workoutId);
+    return record;
+  } catch (error) {
+    throw error;
+  }
+};
+module.exports = { getRecordForWorkout };
