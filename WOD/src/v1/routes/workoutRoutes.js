ed2ed37d8cdef8828c3d188519d07bf98f674cb4
Alik64 <albert.barsamov@gmail.com>
2022-07-05 23:09:57 +0200

    add record GET method in workoutRoutes.js

--- WOD/src/v1/routes/workoutRoutes.js
+++ WOD/src/v1/routes/workoutRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const workoutController = require("../controllers/workoutController");
+const workoutController = require("../controllers/workoutController.js");
+const recordController = require("../controllers/recordController.js");
 
 router.get("/", workoutController.getAllWorkouts);
 router.get("/:workoutId", workoutController.getOneWorkout);
+router.get("/:workoutId/records", recordController.getRecordForWorkout);
+
 router.post("/", workoutController.createNewWorkout);
+
 router.patch("/:workoutId", workoutController.updateOneWorkout);
+
 router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
 module.exports = router;
