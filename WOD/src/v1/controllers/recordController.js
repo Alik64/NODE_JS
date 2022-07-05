7c48d0a5d78f0ca4bee8f1321715d53deb156052
Alik64 <albert.barsamov@gmail.com>
2022-07-05 23:09:21 +0200

    implement getRecordforWorkout function in recordController.js

---
+++ WOD/src/v1/controllers/recordController.js
@@ -0,0 +1,20 @@
+const recordService = require("../services/recordService.js");
+
+const getRecordForWorkout = (req, res) => {
+  const {
+    params: { workoutId },
+  } = req;
+  if (typeof workoutId === "undefined") {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: 'Parameter ":workoutId" can not be empty.' },
+    });
+  }
+  try {
+    const record = recordService.getRecordForWorkout(workoutId);
+    res.send({ status: "OK", data: record });
+  } catch (error) {
+    res.status(400).send({ status: "FAILED", data: error?.message || error });
+  }
+};
+module.exports = { getRecordForWorkout };
