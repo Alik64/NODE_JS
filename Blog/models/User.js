f5b83d8715d177d8d2f1438b17b41daa00dde58b
Alik64 <albert.barsamov@gmail.com>
2022-07-10 16:52:25 +0200

    Create UserSchema

---
+++ Blog/models/User.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    avatarUrl: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", UserSchema);
