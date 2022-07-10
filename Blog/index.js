ef50b61625558869f4b321edd272a82d2752a9c5
Alik64 <albert.barsamov@gmail.com>
2022-07-10 16:29:22 +0200

    connecting to Mongo DB

--- Blog/index.js
+++ Blog/index.js
@@ -1,10 +1,33 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB_KEY)
+  .then(() => {
+    console.log("DB connected with success");
+  })
+  .catch((error) => console.log("DB error : ", error));
 
 const app = express();
 const PORT = process.env.PORT || 4444;
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
+});
+
+app.post("/auth/login", (req, res) => {
+  const { body } = req;
+
+  const token = jwt.sign(
+    { email: body.email, fullName: "John Doe" },
+    "secret123"
+  );
+  console.log("body : ", body);
+  res.json({ success: true, token });
 });
 
 app.listen(PORT, (error) => {
