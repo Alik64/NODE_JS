e1d0ba8e1fcdbb5f8d5736e3e79691efd2aa6a87
Alik64 <albert.barsamov@gmail.com>
2022-08-10 23:51:20 +0200

    implement createWorkout method

--- WOD/src/v1/routes/workoutRoutes.js
+++ WOD/src/v1/routes/workoutRoutes.js
@@ -4,15 +4,15 @@ const recordController = require("../controllers/recordController.js");
 
 const router = express.Router();
 
-router.get("/", workoutController.getAllWorkouts);
-router.get("/random", workoutController.getRandomWorkout);
-router.get("/:workoutId", workoutController.getOneWorkout);
-router.get("/:workoutId/records", recordController.getRecordForWorkout);
+// router.get("/", workoutController.getAllWorkouts);
+// router.get("/random", workoutController.getRandomWorkout);
+// router.get("/:workoutId", workoutController.getOneWorkout);
+// router.get("/:workoutId/records", recordController.getRecordForWorkout);
 
-router.post("/", workoutController.createNewWorkout);
+router.post("/", workoutController.create);
 
-router.patch("/:workoutId", workoutController.updateOneWorkout);
+// router.patch("/:workoutId", workoutController.updateOneWorkout);
 
-router.delete("/:workoutId", workoutController.deleteOneWorkout);
+// router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
 module.exports = router;
