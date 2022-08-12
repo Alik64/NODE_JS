f79860844a4f73157b8bc547361eb4da92b75342
Alik64 <albert.barsamov@gmail.com>
2022-08-13 01:25:18 +0200

    update CRUD for workouts + workoutController.js

--- WOD/src/v1/controllers/workoutController.js
+++ WOD/src/v1/controllers/workoutController.js
@@ -1,5 +1,109 @@
 const WorkoutModel = require("../models/Workout.js");
-const Workout = require("../database/Workout.js");
+
+const getAll = async (req, res) => {
+  try {
+    const workouts = await WorkoutModel.find();
+    res.json(workouts);
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .json({ message: "Impossible to get workouts" });
+  }
+};
+const getOne = async (req, res) => {
+  try {
+    const workoutId = req.params.id;
+    const workout = await WorkoutModel.findById(workoutId);
+    res.json(workout);
+  } catch (error) {
+    console.log("error : ", error);
+    res.status(500).json({
+      message: "Impossible to get workout",
+    });
+  }
+};
+const create = async (req, res) => {
+  try {
+    const doc = new WorkoutModel({
+      name: req.body.name,
+      mode: req.body.mode,
+      timer: req.body.timer,
+      time: req.body.time,
+      exercises: req.body.exercises,
+    });
+
+    const workout = await doc.save();
+    res.json(workout);
+  } catch (error) {
+    console.log("error : ", error);
+    res.status(500).json({
+      message: "Impossible to create a workout",
+    });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const workoutId = req.params.id;
+    WorkoutModel.findOneAndDelete(
+      {
+        _id: workoutId,
+      },
+      (err, doc) => {
+        if (err) {
+          console.log("error : ", err);
+          return res.status(500).json({
+            message: "Impossible to delete workout",
+          });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: "Workout not found",
+          });
+        }
+        res.json({ message: " success" });
+      }
+    );
+  } catch (error) {
+    console.log("error : ", error);
+    res.status(500).json({
+      message: "Impossible to delete Workout",
+    });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const workoutId = req.params.id;
+
+    await WorkoutModel.updateOne(
+      {
+        _id: workoutId,
+      },
+      {
+        name: req.body.name,
+        mode: req.body.mode,
+        timer: req.body.timer,
+        time: req.body.time,
+        exercises: req.body.exercises,
+      }
+    );
+    res.json({ success: true });
+  } catch (error) {
+    console.log("error : ", error);
+    res.status(500).json({
+      message: "Impossible to update a workout",
+    });
+  }
+};
+module.exports = {
+  getAll,
+  // getRandom,
+  getOne,
+  create,
+  update,
+  remove,
+};
 // const getAll = (req, res) => {
 //   const { mode, length, page, equipment } = req.query;
 //   try {
@@ -116,40 +220,3 @@ const Workout = require("../database/Workout.js");
 //       .send({ status: "FAILED", data: { error: error?.message || error } });
 //   }
 // };
-const getAll = async (req, res) => {
-  try {
-    const workouts = await Workout.getAll();
-    res.json(workouts);
-  } catch (error) {
-    res
-      .status(error?.status || 500)
-      .send({ status: "FAILED", data: { error: error?.message || error } });
-  }
-};
-const create = async (req, res) => {
-  try {
-    const doc = new WorkoutModel({
-      name: req.body.name,
-      mode: req.body.mode,
-      timer: req.body.timer,
-      time: req.body.time,
-      exercises: req.body.exercises,
-    });
-
-    const workout = await Workout.create(doc);
-    res.json(workout);
-  } catch (error) {
-    res
-      .status(error?.status || 500)
-      .send({ status: "FAILED", data: { error: error?.message || error } });
-  }
-};
-
-module.exports = {
-  getAll,
-  // getRandom,
-  // getOne,
-  create,
-  // update,
-  // delete
-};
--- WOD/src/v1/routes/workoutRoutes.js
+++ WOD/src/v1/routes/workoutRoutes.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const workoutController = require("../controllers/workoutController.js");
-const recordController = require("../controllers/recordController.js");
 const { workoutCreateValidation } = require("../validations/validations.js");
+const authMiddleware = require("../middleware/auth.middleware.js");
 
 const router = express.Router();
 
 router.get("/", workoutController.getAll);
 // router.get("/random", workoutController.getRandomWorkout);
-// router.get("/:workoutId", workoutController.getOneWorkout);
-// router.get("/:workoutId/records", recordController.getRecordForWorkout);
+router.get("/:id", workoutController.getOne);
 
-router.post("/", workoutCreateValidation, workoutController.create);
+router.post(
+  "/",
+  authMiddleware,
+  workoutCreateValidation,
+  workoutController.create
+);
 
-// router.patch("/:workoutId", workoutController.updateOneWorkout);
+router.patch("/:id", authMiddleware, workoutController.update);
 
-// router.delete("/:workoutId", workoutController.deleteOneWorkout);
+router.delete("/:id", authMiddleware, workoutController.remove);
 
 module.exports = router;
