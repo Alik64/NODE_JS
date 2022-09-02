6e747d2b5bd19cb8b52733f0d89bcd2c68c2a5d3
Alik64 <albert.barsamov@gmail.com>
2022-09-02 14:47:45 +0200

    add getLastTags method

--- Blog/controllers/PostController.js
+++ Blog/controllers/PostController.js
@@ -1,5 +1,21 @@
 import PostModel from "../models/Post.js";
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+    res.json(tags);
+  } catch (error) {
+    console.log("error : ", error);
+    res.status(500).json({
+      message: "Impossible to get posts",
+    });
+  }
+};
+
 export const getAll = async (req, res) => {
   try {
     const posts = await PostModel.find().populate("user").exec();
--- Blog/index.js
+++ Blog/index.js
@@ -62,8 +62,10 @@ app.post(
 );
 
 app.get("/auth/me", checkAuth, UserController.getMe);
+app.get("/tags", PostController.getLastTags);
 
 app.get("/posts", PostController.getAll);
+app.get("/posts/tags", PostController.getLastTags);
 app.get("/posts/:id", PostController.getOne);
 
 app.post(
