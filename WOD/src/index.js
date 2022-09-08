ae099959dc4a7b313812489824bc0bb99e2fe9d3
Alik64 <albert.barsamov@gmail.com>
2022-09-08 17:56:19 +0200

    add app.use(cors())

--- WOD/src/index.js
+++ WOD/src/index.js
@@ -6,8 +6,6 @@ const v1workoutRouter = require("./v1/routes/workoutRoutes.js");
 const v1recordRouter = require("./v1/routes/recordRoutes.js");
 const v1authRouter = require("./v1/routes/authRoutes.js");
 const v1uploadRouter = require("./v1/routes/uploadRoutes.js");
-const authMiddleware = require("./v1/middleware/auth.middleware.js");
-const { upload } = require("./v1/services/static.js");
 
 dotenv.config();
 mongoose
@@ -20,11 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use("/api/v1/workouts", cors(), v1workoutRouter);
-app.use("/api/v1/records", cors(), v1recordRouter);
-app.use("/api/v1/auth", cors(), v1authRouter);
+app.use(cors());
+app.use("/api/v1/workouts", v1workoutRouter);
+app.use("/api/v1/records", v1recordRouter);
+app.use("/api/v1/auth", v1authRouter);
 
-app.use("/api/v1/upload", cors(), v1uploadRouter);
+app.use("/api/v1/upload", v1uploadRouter);
 app.use("/uploads", express.static("uploads"));
 
 app.listen(PORT, (error) => {
